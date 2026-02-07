160c96d2fb2305e9a11857456a39fe3beb30d5ea
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-07 15:41:26 -0300

    :building_construction: chore: configure drizzle orm and database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,1 +1,11 @@
+import { defineConfig } from "drizzle-kit"
+import "dotenv/config"
 
+export default defineConfig({
+    schema: "./src/db/schema.js",
+    out: "./src/db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!
+    }
+})
