3b763a620373a9c602837596388f3ad157495dc7
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-07 16:48:48 -0300

    :sparkles: feat: create mainRouter

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import { globalError } from "./middlewares/global-error.js"
+import mainRouter from "./routes/index.js"
 
 const app = express()
 
 app.use(express.json())
+
+app.use(mainRouter)
 
 app.use(globalError)
 
