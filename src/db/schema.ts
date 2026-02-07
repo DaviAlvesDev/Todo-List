1514b5cf72816575758b53e47f6fd26af3dfc9ee
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-07 16:06:59 -0300

    :card_file_box: feat(db): create table auth, users and tasks

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey(),
+    name: text("name").notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    createdAt: timestamp("created_at").defaultNow()
+})
+
+export const tasks = pgTable("tasks", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    description: text("description"),
+    dueDate: timestamp("due_date"),
+    userID: uuid("user_id").notNull()
+        .references(() => users.id, {
+            onDelete: "cascade",
+            onUpdate: "cascade"
+        }),
+    createdAt: timestamp("created_at").defaultNow()
+})
