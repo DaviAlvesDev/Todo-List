45bc6471abc27ac8afa45cec3eeb3315805e6e48
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-08 09:13:56 -0300

    🥅 feat: add AuthError case in globalErrors

--- src/middlewares/global-error.ts
+++ src/middlewares/global-error.ts
@@ -1,15 +1,33 @@
 import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/app-error.js";
+import { AuthError } from "@supabase/supabase-js";
 
 export function globalError(err: any, req: Request, res: Response, next: NextFunction) {
     if (err instanceof AppError) {
         if (err.statusCode == 500) {
-            console.error(`[${req.method}] ${req.url} - error: ${err}`)
+            console.error(`[${req.method}] ${req.url} - error: ${err.message}`)
+            if (err.stack) console.error(err.stack)
         }
         return res.status(err.statusCode)
             .json({
                 ok: false,
                 msg: err.message
+            })
+    }
+
+    if (err instanceof AuthError) {
+        const statusCode = err.status && err.status >= 100 && err.status <= 600 ?
+            err.status : 500
+
+        if (statusCode >= 500) {
+            console.error(`[${req.method}] ${req.url} - error: ${err}`)
+            if (err.stack) console.error(err.stack)
+        }
+
+        return res.status(statusCode)
+            .json({
+                ok: false,
+                msg: statusCode < 500 ? err.message : "Internal auth error"
             })
     }
 
