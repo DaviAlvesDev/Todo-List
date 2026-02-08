6176ddda54af64bb5975378e31b78e86af711192
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-08 09:12:01 -0300

    🗃 feat(db): create column "role" in users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,8 @@ export const users = pgTable("users", {
     id: uuid("id").primaryKey(),
     name: text("name").notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
-    createdAt: timestamp("created_at").defaultNow()
+    createdAt: timestamp("created_at").defaultNow(),
+    role: text("role").default("user")
 })
 
 export const tasks = pgTable("tasks", {
