9de81f100af1d763ce3f5b98465b60a4032266dd
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-08 10:33:21 -0300

    :sparkles: feat: create auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import usersRouter from "./users.js"
 import { auth } from "../middlewares/auth.js"
+import authRouter from "./auth.js"
 
 const mainRouter = Router()
 
 mainRouter.use("/users", auth, usersRouter)
+
+mainRouter.use("/auth", authRouter)
 
 export default mainRouter
